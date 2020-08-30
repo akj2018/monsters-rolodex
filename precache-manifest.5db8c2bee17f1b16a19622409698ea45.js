5203da2843285e833487f4e6074e21f9d0640d7d
Akshay Jain <jainakshay1783@gmail.com>
2020-08-30 14:38:02 +0530

    Updates

---
+++ precache-manifest.5db8c2bee17f1b16a19622409698ea45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089036973e728dda5167643830fb9403",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "247b04ec4717970cd552",
+    "url": "/monsters-rolodex/static/css/main.95a768e9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247b04ec4717970cd552",
+    "url": "/monsters-rolodex/static/js/main.2105cf56.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
